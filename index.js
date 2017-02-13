6ae82d379250bb3ef47db08f43b13bde95e0c74c
Tomasz Martyński <tomek@martynski.pl>
2017-02-13 22:41:07 +0100

    removed unnecessary ".js" from require statements

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ var impl = null;
 
 switch(os.type()) {
   case 'Darwin':
-    impl = require('./impl/darwin.js');
+    impl = require('./impl/darwin');
     break;
   case 'Linux':
-    impl = require('./impl/linux.js');
+    impl = require('./impl/linux');
     break;
   default:
     throw new Error('Your OS is currently not supported by node-loudness.');
